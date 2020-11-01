9dd18e1222c79a11854703f62a375a6510a5a5ac
Miroslav <mirchev1977@yahoo.com>
2020-11-01 05:53:21 +0200

    create Profile record in DB and redirect

--- app/views/index.py
+++ app/views/index.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 
 from app.forms.profiles import ProfileForm
 from app.models import Profile
+from app.views.profiles import create_profile
 
 
 def index(request):
     if Profile.objects.exists():
         pass
     else:
-        context = {
-            'form': ProfileForm()
-        }
-        return render(request, 'home-no-profile.html', context)
+        return create_profile(request);
--- app/views/profiles.py
+++ app/views/profiles.py
@@ -1,8 +1,27 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from app.forms.profiles import ProfileForm
+
+
 def profile_index(request):
     pass
 
 def create_profile(request):
-    pass
+    if request.method == 'GET':
+        context = {
+            'form': ProfileForm()
+        }
+        return render(request, 'home-no-profile.html', context)
+    else:
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        context = {
+            'form': ProfileForm()
+        }
+        return render(request, 'home-no-profile.html', context)
 
 def edit_profile(request):
     pass
