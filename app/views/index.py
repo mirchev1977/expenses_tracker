593cad68f59ac5fc52fc45a1a321fea756e7b905
Miroslav <mirchev1977@yahoo.com>
2020-11-01 07:17:07 +0200

    render home-with-profile.html with context of profile and expenses

--- app/views/index.py
+++ app/views/index.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 from app.forms.profiles import ProfileForm
-from app.models import Profile
+from app.models import Profile, Expense
 from app.views.profiles import create_profile
 
 
 def index(request):
     if Profile.objects.exists():
-        return render(request, 'home-with-profile.html')
+        context = {
+            'profile':  Profile.objects.all()[0],
+            'expenses': Expense.objects.all(),
+        }
+        return render(request, 'home-with-profile.html', context)
     else:
         return create_profile(request);
